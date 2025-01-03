65b73b8029ef621f9173fceb8bd180090398f538
Hadaly <hadalyvf@gmail.com>
2025-01-03 13:34:24 +0100

    refactor: data display

--- src/js/dataDisplay.js
+++ src/js/dataDisplay.js
@@ -17,10 +17,18 @@ document.addEventListener("DOMContentLoaded", function () {
     { url: '../data/loader_gltf.json', elementId: 'obj-gltf' }, 
     
     // MODEL GLTF // INSTANCE
+    { url: '../data/instance.json', elementId: 'obj-inst' },
+
+    { url: '../data/instance_parser.json', elementId: 'obj-inst-parser' },
     { url: '../data/instance_parser_extensions.json', elementId: 'obj-inst-ext' },
+    { url: '../data/instance_parser_extensions_draco.json', elementId: 'obj-inst-ext-draco' },
+    { url: '../data/instance_parser_extensions_binary.json', elementId: 'obj-inst-ext-binary' },
+    { url: '../data/instance_parser_plugins.json', elementId: 'obj-inst-parser-plugins' },
+
     { url: '../data/instance_geometry.json', elementId: 'obj-inst-geo' },
     { url: '../data/instance_material.json', elementId: 'obj-inst-mat' },
-    { url: '../data/instance_parser.json', elementId: 'obj-inst-parser' },
+    { url: '../data/instance_mesh.json', elementId: 'obj-inst-mesh' },
+
 
     // GPU COMPUTATION
     { url: '../data/gpu_computation.json', elementId: 'obj-gpu-comp' },
