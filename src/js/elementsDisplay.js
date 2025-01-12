29f6f9693b73eb6878cf0780e9d50f230e4a1ada
Hadaly <hadalyvf@gmail.com>
2025-01-12 01:34:09 +0100

    fix: button events

--- src/js/elementsDisplay.js
+++ src/js/elementsDisplay.js
@@ -1,44 +1,38 @@
 document.addEventListener('DOMContentLoaded', init);
+const dataButton = document.getElementById('btn-toggle-data');
+const ctrlButton = document.getElementById('btn-toggle-ctrl');
+const infoButton = document.getElementById('btn-toggle-info');
+
+const panelInfo = document.getElementById('panel-info');
+const panelControls = document.getElementById('panel-controls');
+
 
 function init() {
-  const dataButton = document.getElementById('btn-toggle-data');
-  const ctrlButton = document.getElementById('btn-toggle-ctrl');
-  const infoButton = document.getElementById('btn-toggle-info');
+  document.addEventListener('click', clickOutsideHandler);
 
   if (dataButton) {
     dataButton.addEventListener('click', toggleData);
   }
 
   if (ctrlButton) {
-    ctrlButton.addEventListener('click', function() {
-      togglePanel('panel-controls', 'panel-info');
-      toggleButtonSelectedState('btn-toggle-ctrl', 'btn-toggle-info');
-
+    ctrlButton.addEventListener('click', function (event) {
+      togglePanel(panelControls, panelInfo);
+      toggleButtonSelected(ctrlButton, infoButton);
+      event.stopPropagation();
     });
   }
 
   if (infoButton) {
-    infoButton.addEventListener('click', function() {
-      togglePanel('panel-info', 'panel-controls');
-      toggleButtonSelectedState('btn-toggle-info', 'btn-toggle-ctrl');
+    infoButton.addEventListener('click', function (event) {
+      togglePanel(panelInfo, panelControls);
+      toggleButtonSelected(infoButton, ctrlButton);
+      event.stopPropagation();
     });
   }
 }
 
-function selectButton(buttonId) {
-  const button = document.getElementById(buttonId);
-  button.classList.add('selected');
-}
 
-function unselectButton(buttonId) {
-  const button = document.getElementById(buttonId);
-  button.classList.remove('selected');
-}
-
-function toggleButtonSelectedState(activeButtonId, otherButtonId) {
-  const activeButton = document.getElementById(activeButtonId);
-  const otherButton = document.getElementById(otherButtonId);
-
+function toggleButtonSelected(activeButton, otherButton) {
   if (activeButton) {
     activeButton.classList.toggle('selected');
   }
@@ -48,81 +42,45 @@ function toggleButtonSelectedState(activeButtonId, otherButtonId) {
   }
 }
 
-function togglePanel(currentPanelId, otherPanelId) {
-  const currentPanel = document.getElementById(currentPanelId);
-  const otherPanel = document.getElementById(otherPanelId);
-
+function togglePanel(currentPanel, otherPanel) {
   if (currentPanel && otherPanel) {
     if (currentPanel.classList.contains('hide')) {
-      showPanel(currentPanelId);
-      hidePanel(otherPanelId);
+      currentPanel.classList.remove('hide');
+      otherPanel.classList.add('hide');
     } else {
-      hidePanel(currentPanelId);
+      currentPanel.classList.add('hide');
     }
   }
 }
 
-function hidePanel(panelId) {
-  const panel = document.getElementById(panelId);
-  if (panel && !panel.classList.contains('hide')) {
-    panel.classList.add('hide');
-  }
-}
-
-function showPanel(panelId) {
-  const panel = document.getElementById(panelId);
-  if (panel && panel.classList.contains('hide')) {
-    panel.classList.remove('hide');
-  }
-}
-
 function toggleData() {
-  const button = document.getElementById('btn-toggle-data');
-  const elements = document.querySelectorAll('.data-tgl');
-  const clonedContainers = document.querySelectorAll('.s-data-clnd');
-  let isHidden = true;
+  const elements = document.querySelectorAll('.data-tgl, .s-data-clnd');
+  let isHidden = false;
 
   elements.forEach(element => {
     if (element.classList.contains('hide')) {
       element.classList.remove('hide');
     } else {
       element.classList.add('hide');
-      isHidden = false;
+      isHidden = true;
     }
   });
 
-  clonedContainers.forEach(container => {
-    if (container.classList.contains('hide')) {
-      container.classList.remove('hide');
-    } else {
-      container.classList.add('hide');
-      isHidden = false;
-    }
-  });
-
-  if (!isHidden) {
-    button.textContent = 'Reveal Data';
-    disableButton('btn-toggle-ctrl', true);
-    togglePanel('panel-controls', 'panel-info');
-    selectButton('btn-toggle-ctrl');
-    unselectButton('btn-toggle-info');
+  if (isHidden) {
+    dataButton.textContent = 'Reveal Data';
   } else {
-    button.textContent = 'Hide Data';
-    disableButton('btn-toggle-ctrl', false);
-    hidePanel('panel-controls');
-    unselectButton('btn-toggle-ctrl');
+    dataButton.textContent = 'Hide Data';
   }
 }
 
-function disableButton(buttonId, disabled) {
-  const button = document.getElementById(buttonId);
-  if (button) {
-    if (!disabled) {
-      button.disabled = true;
-      button.classList.add('disabled');
-    } else {
-      button.disabled = false;
-      button.classList.remove('disabled');
+function clickOutsideHandler(event) {
+  if (!panelInfo.contains(event.target) && !panelControls.contains(event.target) &&
+    !dataButton.contains(event.target) && !ctrlButton.contains(event.target) && !infoButton.contains(event.target)) {
+    if (!panelInfo.classList.contains('hide')) {
+      panelInfo.classList.add('hide');
+    }
+    if (!panelControls.classList.contains('hide')) {
+      panelControls.classList.add('hide');
     }
   }
 }
